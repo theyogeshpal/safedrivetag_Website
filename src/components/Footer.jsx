ef843e8d54ec1a2780140f1d8d39c62487fc5f77
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-08-24 21:26:16 +0530

    Remove white SVG wave divider and margin above footer to seamlessly merge orange CTA with black footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,15 +32,7 @@ export default function Footer() {
       <p>Secured by SafeDrive-Tag &copy; {new Date().getFullYear()}</p>
     </footer>
   ) : (
-    <footer className="relative bg-[#000000] text-white/80 pt-24 pb-12 overflow-hidden mt-10">
-      
-      {/* Decorative Wave/Curve at Top */}
-      <div className="absolute top-0 left-0 w-full overflow-hidden leading-none">
-        <svg className="relative block w-full h-[60px]" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="fill-white"></path>
-        </svg>
-      </div>
-
+    <footer className="relative bg-[#000000] text-white/80 pt-16 pb-12 overflow-hidden border-t border-white/10">
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-12 relative z-10">
         
         {/* Brand Column */}
